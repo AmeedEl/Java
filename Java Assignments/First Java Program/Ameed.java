5f4fc150613635b6550e975def43fbf0650b29ca
AmeedEl <ameedvevo@gmail.com>
2024-12-03 13:41:55 +0200

    First Java Assignment

---
+++ Java Assignments/First Java Program/Ameed.java
@@ -0,0 +1,7 @@
+public class Ameed{
+    public static void main(String[] args) {
+	System.out.println("It's Ameed!");
+	System.out.println("I'm 26 years old!");
+	System.out.println("I'm Tubasian originally and currently I'm in Ramallah, Palestine.");
+    }
+}
